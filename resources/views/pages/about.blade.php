d0417a9cfa9b327d5f370bc373dc23b89b062185
Bruce Ong <bruceskellator@hotmail.com>
2021-03-09 17:29:02 +0800

    Added basic styling for each route view as well as navbar.

--- resources/views/pages/about.blade.php
+++ resources/views/pages/about.blade.php
@@ -1,12 +1,6 @@
-<!DOCTYPE html>
-<html lang="{{ str_replace('_', '-', app()->getLocale()) }}">
-    <head>
-        <meta charset="utf-8">
-        <meta name="viewport" content="width=device-width, initial-scale=1">
-        <title>{{ config('app.name', 'Laravel App') }}</title>
-    </head>
-    <body class="antialiased">
-        <h1>About</h1>
-        <p>This is the about page</p>
-    </body>
-</html>
+@extends('layouts.app')
+
+@section('content')
+    <h1>{{ $title }}</h1>
+    <p>This is the about page</p>
+@endsection
